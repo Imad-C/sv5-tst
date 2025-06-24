be4d2f790c498c7c9b5f040df99d2cd8f9453183
imad-chowdhury-dluhc <Imad.Chowdhury1@communities.gov.uk>
2025-06-24 11:20:25 +0100

    Trimmed Todo component

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,19 +1,6 @@
 <script lang="ts">
-	import { getTodosState } from '../routes/todo/todo.svelte';
-
 	let { todo } = $props();
-
-	const todos = getTodosState();
-
-	function updateCompleted() {
-		todos.updateTodo(todo.id, { completed: !todo.completed });
-	}
 </script>
 
 <p>ID: {todo.id} | Text: {todo.text} | Completed: {todo.completed}</p>
-<button
-	onclick={() => {
-		updateCompleted();
-	}}>Update Completed</button
->
 <input type="checkbox" bind:checked={todo.completed} />
